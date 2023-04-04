28fd77cb229ee1f640338231c8e57c60a6d4ed0b
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-04-04 11:50:01 +0200

    feat: add dbConnection; display Top 3 on index

--- src/index.php
+++ src/index.php
@@ -1,3 +1,25 @@
-<?php include('inc/header.php'); ?>
-<h1>ATP Ranking</h1>
+<?php
+require('lib/dbConnect.php');
+
+$pdo = dbConnect();
+$sql = "SELECT * FROM players WHERE rank BETWEEN 1 AND 3 ORDER BY rank";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+include('inc/header.php'); ?>
+<h1>Top 3 Players</h1>
+<?php foreach ($result as $player) { ?>
+    <div class="card">
+        <div class="card-header">
+            <h2><?= $player["firstname"] . " " . $player["lastname"] ?></h2>
+        </div>
+        <div class="card-body">
+            <div class="card-text"><?= $player["age"] ?></div>
+            <div class="card-text"></div>
+            <div class="card-text"></div>
+            <div class="card-text"></div>
+        </div>
+    </div>
+<?php } ?>
 <?php include('inc/footer.php');
--- src/lib/guidv4.php
+++ src/lib/guidv4.php
@@ -1,5 +1,6 @@
 <?php
-function guidv4($data = null) {
+function guidv4($data = null): string
+{
     // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
     $data = $data ?? random_bytes(16);
     assert(strlen($data) == 16);
