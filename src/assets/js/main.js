df837b70ad598eb98e2b2db80d0b03097c7bebc1
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-04-05 16:37:59 +0200

    fix: remove intempestive console.log

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,6 @@
 const tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
 );
-console.log(tooltipTriggerList);
 const tooltipList = [...tooltipTriggerList].map(
   (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
