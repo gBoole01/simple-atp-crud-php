584442feeed19c7f17bb4b300d0961a51ea24bec
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-04-07 09:11:57 +0200

    feat: add tooltip on buttons

--- src/rankings.php
+++ src/rankings.php
@@ -37,7 +37,7 @@ include('inc/header.php'); ?>
                         <td><?= $player["age"] ?></td>
                         <td><?= $player["points"] ?></td>
                         <td>
-                            <a class="btn btn-success" href="/viewPlayer.php?id=<?= $player["uuid"] ?>">
+                            <a class="btn btn-success" href="/viewPlayer.php?id=<?= $player["uuid"] ?>" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="See more">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                                     <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
                                     <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
